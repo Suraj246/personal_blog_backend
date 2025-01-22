82a2fe5d2bcacb628cd007905813b0115a0b6dad
suraj <surajsurwase9881@gmail.com>
2025-01-22 11:46:22 +0530

    there was a problem with uploading file but now it is resolve i think

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,11 +6,11 @@ import newUser from '../schema/userSchema.js';
 import mongoose from 'mongoose';
 
 import path from 'path'
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
 // Convert import.meta.url to a file path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); // Define __dirname
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename); // Define __dirname
 
 // accessing the uploads directory
 // console.log("path", path.join(__dirname, '../uploads'))
@@ -18,7 +18,8 @@ const __dirname = path.dirname(__filename); // Define __dirname
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads'))
+        cb(null, path.resolve('./uploads'))
+        // cb(null, path.join(__dirname, '../uploads'))
     },
     filename: function (req, file, cb) {
         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
